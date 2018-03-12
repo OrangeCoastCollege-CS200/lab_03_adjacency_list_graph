0fd72d65204b725bd7305285ac0f32213a115529
Severiano Badajoz <sevebadajoz@gmail.com>
2018-03-11 22:31:21 -0700

    Edit main method

--- Project1/Main.cpp
+++ Project1/Main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main()
 {
-	vector<vector<string>> graphData =
+	vector<vector<string>> data =
 	{
 		{ "A", "B", "F" },
 	{ "B", "C", "D", "F" },
@@ -16,175 +16,179 @@ int main()
 	{ "G" },
 	};
 
-	// TESTING DEFAULT CONSTRUCTOR
-	cout << "TESTING DEFAULT CONSTRUCTOR\n";
-	AdjListGraph defGraph;
-	printGraphInfo(defGraph);
-	cout << "\n";
+	cout << "DEFAULT CONSTRUCTOR" << endl;
+	AdjListGraph graph1;
+	print(graph1);
+	cout << endl;
 
-	// TESTING OVERLOADED CONSTRUCTOR
-	cout << "TESTING OVERLOADED CONSTRUCTOR\nParameter: 7\n";
-	AdjListGraph ovGraph(7);
-	printGraphInfo(ovGraph);
-	cout << "\n";
+	cout << "OVERLOADED CONSTRUCTOR" << endl << "Parameter: 7" << endl;
+	AdjListGraph graph2(7);
+	print(graph2);
+	cout << endl;
 
-	// TESTING CREATEGRAPH
-	cout << "TESTING CREATEGRAPH\n-------Before-------\n";
-	printGraphInfo(ovGraph);
-	cout << "-------After-------\n";
-	ovGraph.createGraph(graphData);
-	printGraphInfo(ovGraph);
-	cout << "\n";
-	// Error when Graph data has more vertices than Graph object max vertices
-	cout << "TESTING CREATEGRAPH (FAILURE)\n-------Before-------\n";
-	AdjListGraph failGraph(1);
-	printGraphInfo(failGraph);
-	cout << "-------After-------\n";
-	failGraph.createGraph(graphData);
-	printGraphInfo(failGraph);
-	cout << "\n";
+	cout << "CREATE GRAPH" << endl; 
+	cout << "-Before--" << endl;
+	print(graph2);
+	cout << "-After-" << endl;
+	graph2.createGraph(data);
+	print(graph2);
+	cout << endl;
 
-	// TESTING COPY CONSTRUCTOR
-	cout << "TESTING COPY CONSTRUCTOR\n-------Before-------\n";
-	cout << "**Copy from**\n";
-	printGraphInfo(ovGraph);
-	cout << "-------After-------\n";
-	AdjListGraph copyGraph(ovGraph);
-	cout << "**New copy**\n";
-	printGraphInfo(copyGraph);
-	cout << "**Copy from**\n";
-	printGraphInfo(ovGraph);
-	cout << "\n";
+	cout << "CREATE GRAPH(ERROR)" << endl; 
+	cout << "-Before-" << endl;
+	AdjListGraph graph3(1);
+	print(graph3);
+	cout << "-After-" << endl;
+	graph3.createGraph(data);
+	print(graph3);
+	cout << endl;
 
-	// TESTING OVERLOADED ASSIGNMENT
-	// Assign graph to graph with more vertices.
-	cout << "TESTING OVERLOADED ASSIGNMENT (ASSIGN MORE VTXS)\n-------Before-------\n";
-	cout << "**Assign to**\n";
-	printGraphInfo(defGraph);
-	cout << "**Assign from**\n";
-	printGraphInfo(ovGraph);
-	cout << "-------After-------\n";
-	defGraph = ovGraph;
-	cout << "**Assign to**\n";
-	printGraphInfo(defGraph);
-	cout << "**Assign from**\n";
-	printGraphInfo(ovGraph);
-	cout << "\n";
-	// Assign graph to graph with less vertices.
-	cout << "TESTING OVERLOADED ASSIGNMENT (ASSIGN LESS VTXS)\n-------Before-------\n";
-	cout << "**Assign to**\n";
-	printGraphInfo(defGraph);
-	cout << "**Assign from**\n";
-	printGraphInfo(failGraph);
-	cout << "-------After-------\n";
-	defGraph = failGraph;
-	cout << "**Assign to**\n";
-	printGraphInfo(defGraph);
-	cout << "**Assign from**\n";
-	printGraphInfo(failGraph);
-	cout << "\n";
+	cout << "COPY CONSTRUCTOR" << endl; 
+	cout << "-Before-" << endl;
+	cout << "other: ";
+	print(graph2);
+	cout << "-After-" << endl;
+	AdjListGraph copyGraph(graph2);
+	cout << "new: ";
+	print(copyGraph);
+	cout << "other: ";
+	print(graph2);
+	cout << endl;
 
-	// TESTING MOVE CONSTRUCTOR
-	cout << "TESTING MOVE CONSTRUCTOR\n-------Before-------\n";
-	cout << "**Move from**\n";
-	printGraphInfo(ovGraph);
-	cout << "-------After-------\n";
-	AdjListGraph moveGraph(move(ovGraph));
-	cout << "**New Graph**\n";
-	printGraphInfo(moveGraph);
-	cout << "**Move from**\n";
-	printGraphInfo(ovGraph);
-	cout << "\n";
 
-	// TESTING MOVE ASSIGNMENT
-	// Assign graph to graph with more vertices.
-	cout << "TESTING MOVE ASSIGNMENT (ASSIGN MORE VTXS)\n-------Before-------\n";
-	cout << "**Move to**\n";
-	printGraphInfo(ovGraph);
-	cout << "**Move from**\n";
-	printGraphInfo(moveGraph);
-	cout << "-------After-------\n";
-	ovGraph = move(moveGraph);
-	cout << "**Move to**\n";
-	printGraphInfo(ovGraph);
-	cout << "**Move from**\n";
-	printGraphInfo(moveGraph);
-	cout << "\n";
-	// Assign graph to graph with less vertices.
-	cout << "TESTING MOVE ASSIGNMENT (ASSIGN LESS VTXS)\n-------Before-------\n";
-	cout << "**Move to**\n";
-	printGraphInfo(ovGraph);
-	cout << "**Move from**\n";
-	printGraphInfo(moveGraph);
-	cout << "-------After-------\n";
-	ovGraph = move(moveGraph);
-	cout << "**Move to**\n";
-	printGraphInfo(ovGraph);
-	cout << "**Move from**\n";
-	printGraphInfo(moveGraph);
-	cout << "\n";
+	cout << "OVERLOADED ASSIGNMENT(> VERTEXES)" << endl;
+	cout << "-Before-" << endl;
+	cout << "calling:";
+	print(graph1);
+	cout << "other: ";
+	print(graph2);
+	cout << "-After-" << endl;
+	graph1 = graph2;
+	cout << "calling: ";
+	print(graph1);
+	cout << "other: ";
+	print(graph2);
+	cout << endl;
 
-	// TESTING DESTRUCTOR
+	cout << "OVERLOADED ASSIGNMENT (< VERTEXES)" << endl; 
+	cout << "-Before-" << endl;
+	cout << "calling: " << endl;
+	print(graph1);
+	cout << "other: " << endl;
+	print(graph3);
+	cout << "-After-" << endl;
+	graph1 = graph3;
+	cout << "calling: " << endl;
+	print(graph1);
+	cout << "other: " << endl;
+	print(graph3);
+	cout << endl;
+
+	cout << "MOVE CONSTRUCTOR" << endl; 
+	cout << "-Before-" << endl;
+	cout << "other: ";
+	print(graph2);
+	cout << "-After-" << endl;
+	AdjListGraph moveGraph(move(graph2));
+	cout << "new: ";
+	print(moveGraph);
+	cout << "other: ";
+	print(graph2);
+	cout << endl;
+
+	cout << "MOVE ASSIGNMENT (ASSIGN MORE VTXS)" << endl; 
+	cout << "-Before-" << endl;
+	cout << "calling: ";
+	print(graph2);
+	cout << "other: ";
+	print(moveGraph);
+	cout << "-After-" << endl;
+	graph2 = move(moveGraph);
+	cout << "calling: ";
+	print(graph2);
+	cout << "other: ";
+	print(moveGraph);
+	cout << endl;
+
+	cout << "TESTING MOVE ASSIGNMENT (ASSIGN LESS VTXS)" << endl; 
+	cout << "-Before-" << endl;
+	cout << "calling: ";
+	print(graph2);
+	cout << "other: ";
+	print(moveGraph);
+	cout << "-After-" << endl;
+	graph2 = move(moveGraph);
+	cout << "calling: ";
+	print(graph2);
+	cout << "other: ";
+	print(moveGraph);
+	cout << endl;
+
 	AdjListGraph * destGraph = new AdjListGraph(7);
-	destGraph->createGraph(graphData);
-	cout << "TESTING DESTRUCTOR\n-------Before-------\n";
-	printGraphInfo(*destGraph);
-	cout << "-------After-------\n";
+	destGraph->createGraph(data);
+	cout << "DESTRUCTOR" << endl; 
+	cout << "-Before-" << endl;
+	print(*destGraph);
+	cout << "-After-" << endl;
 	delete destGraph;
-	printGraphInfo(*destGraph);
+	print(*destGraph);
 	destGraph = nullptr;
-	cout << "\n";
+	cout << endl;
 
-	// TESTING INSERTVERTEX
-	cout << "TESTING INSERTVERTEX\n-------Original-------\n";
-	printGraphInfo(copyGraph);
-	cout << "--- Adding vertex H (with successors A and C) ---\n";
+
+	cout << "INSERT VERTEX" << endl; 
+	cout << "-Original-" << endl;
+	print(copyGraph);
+	cout << "Adding vertex H (with successors A and C)" << endl;
 	copyGraph.insertVertex("H", vector<string>(), { "A", "C" });
-	printGraphInfo(copyGraph);
-	cout << "--- Adding vertex I (with predecessors B and D) ---\n";
+	print(copyGraph);
+	cout << "Adding vertex I (with predecessors B and D)" << endl;
 	copyGraph.insertVertex("I", { "B", "D" }, vector<string>());
-	printGraphInfo(copyGraph);
-	cout << "--- Adding vertex J (with predecessors E and G and successors F and H) ---\n";
+	print(copyGraph);
+	cout << "Adding vertex J (with predecessors E and G and successors F and H)" << endl;
 	copyGraph.insertVertex("J", { "E", "G" }, { "F", "H" });
-	printGraphInfo(copyGraph);
-	cout << "--- Attempting to add vertex J again ---\n";
+	print(copyGraph);
+	cout << "Attempting to add vertex J again" << endl;
 	copyGraph.insertVertex("J", { "E", "G" }, { "F", "H" });
-	printGraphInfo(copyGraph);
-	cout << "--- Adding verticies K, L, M, and N (no predecessors or successors) ---\n";
+	print(copyGraph);
+	cout << "Adding verticies K, L, M, and N (no predecessors or successors)" << endl;
 	copyGraph.insertVertex("K", vector<string>(), vector<string>());
 	copyGraph.insertVertex("L", vector<string>(), vector<string>());
 	copyGraph.insertVertex("M", vector<string>(), vector<string>());
 	copyGraph.insertVertex("N", vector<string>(), vector<string>());
-	printGraphInfo(copyGraph);
-	cout << "--- Attempting to add vertex O (already at max vertices) ---\n";
+	print(copyGraph);
+	cout << "Attempting to add vertex O (already at max vertices)" << endl;
 	copyGraph.insertVertex("O", vector<string>(), vector<string>());
-	printGraphInfo(copyGraph);
-	cout << "\n";
+	print(copyGraph);
+	cout << endl;
 
-	// TESTING TRAVERSE
-	cout << "TESTING TRAVERSE\nGraph to traverse:\n";
-	printGraphInfo(copyGraph);
+
+	cout << "TRAVERSE" << endl; 
+	cout << "Graph to traverse:" << endl;
+	print(copyGraph);
 	cout << "Traversing: ";
 	vector<string> travGraphVect = copyGraph.traverse();
 	for (string vtx : travGraphVect) cout << vtx << " ";
-	cout << "\n";
+	cout << endl;
 
-	// TESTING EMPTYGRAPH
-	cout << "TESTING EMPTYGRAPH\n-------Before-------\n";
-	printGraphInfo(copyGraph);
-	cout << "-------After-------\n";
+
+	cout << "EMPTY GRAPH" << endl; 
+	cout << "-Before-" << endl;
+	print(copyGraph);
+	cout << "-After-" << endl;
 	copyGraph.emptyGraph();
-	printGraphInfo(copyGraph);
-	cout << "\n";
+	print(copyGraph);
+	cout << endl;
 
 	cout << endl;
 	system("Pause");
 	return 0;
 }
 
-void printGraphInfo(const AdjListGraph& graph)
+void print(const AdjListGraph& graph)
 {
-	cout << graph << "\nNumber of vertices: " << graph.n()
-		<< "\nMax vertices: " << graph.getMaxVertices()
-		<< "\nAddress: " << &graph << "\n";
+	cout << graph << endl 
+		<< "Number of vertices= " << graph.getNumVerticies()
+		<< "Capacity= " << graph.getCapacity() << endl
+		<< "Memory= " << &graph << endl;
 }

dd5fd996f8914d30d1692996c89e9b0d9253bb6c
Severiano Badajoz <sevebadajoz@gmail.com>
2018-03-11 19:14:35 -0700

    Add methods and member variables to AdjListGraph.h

--- Project1/AdjListGraph.h
+++ Project1/AdjListGraph.h
@@ -3,13 +3,44 @@
 
 #include<iostream>
 #include <string>
+#include <vector>
+#include "AnyList.h"
 using namespace std;
 
 class AdjListGraph {
 public:
+	AdjListGraph();
+		//default constructor
+
+	AdjListGraph(int);
+		//parameterized constructor
+
+	AdjListGraph(const AdjListGraph&);
+		//copy constructor
+
+	AdjListGraph(AdjListGraph&&);
+		//move constructor
+
+	AdjListGraph& operator=(const AdjListGraph&);
+		//overloaded copy assignment operator
+
+	AdjListGraph& operator=(AdjListGraph&&);
+		//overloaded move assignment operator
+
+	void createGraph(const vector<vector<string>>& data);
+	void insertVertex(const string& newVertex, const vector<string>& predList,
+		const vector<string>& succList);
+
+	void emptyGraph();
+	~AdjListGraph();
+	
+
 
 private:
-
+	string * verticies;
+	AnyList * graph;
+	int numVerticies;
+	int capacity;
 };
 
 #endif // !ADJLISTGRAPH_H
